31457617778f9b64dbd94c3cbf61edd85195aa94
Ginevra Cepparulo <cepparuloginevra@gmail.com>
2025-04-10 11:54:15 +0200

    Changed openai key

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ GOOGLE_CSE_ID = os.environ.get("GOOGLE_CSE_ID", "64c807de4a9d1425d")
 # Initialize Groq client
 groq_client = Groq(api_key=GROQ_API_KEY)
 
-OPEN_AI_KEY = "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"
+OPEN_AI_KEY = "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"
 os.environ["OPENAI_API_KEY"] = OPEN_AI_KEY
 # Constants openai/gpt-4.5-preview openai/gpt-3.5-turbo
 MODEL_NAME = "llama-3.3-70b-versatile"  
